b1fc4ed47fd4ef5e0f51daba617117b907ebf8cb
usuario <usuario@MacBook-Pro-de-propietario.local>
2023-01-03 13:47:05 -0600

    version con matriz

---
+++ js/controller.js
@@ -0,0 +1,72 @@
+$(function(){
+
+    $("#createMatriz").on("click", (event) => {
+
+        let vertices = parseInt($("#vertices").val());
+        let aristas = parseInt($("#aristas").val());
+        $("#tableMatriz").html("");
+
+
+        let matriz = [];
+
+        for(let x=0; x<vertices; x++){
+            let xv = [];
+            for(let y=0; y<vertices; y++){
+                xv.push(0);
+            }
+            matriz.push(xv);
+        }
+    
+        matriz = llenarAristas(matriz, vertices, aristas);
+
+        for(let x=0; x<vertices; x++) {
+            let tr = $("<tr>");
+            for(let y=0; y<vertices; y++) {
+                let td = $("<td>");
+                $(td).html(matriz[x][y]);
+
+                if(y==0){
+                    $(td).addClass("border-start");
+                }
+
+                if(y==(vertices-1)){
+                    $(td).addClass("border-end");
+                }
+
+                $(td).addClass("p-1").addClass("text-center");
+                $(tr).append(td);
+            }
+            $("#tableMatriz").append(tr);
+        }
+
+    });    
+
+    function llenarAristas(matrizVertices, vertices, aristas){
+        if(aristas>(vertices*vertices)){
+
+        }
+        let c=1;
+        while(c<=aristas){
+            let x = aleatorio(vertices);
+            let y = aleatorio(vertices);
+            if(matrizVertices[x-1][y-1]==0){
+                matrizVertices[x-1][y-1] = 1;
+                c += 1;
+            }
+        }
+        return matrizVertices;
+
+    }
+
+    function aleatorio(vertices){
+        var x = Math.floor(Math.random()*vertices)+1;
+        return x;
+    }
+
+
+
+
+
+
+
+})
